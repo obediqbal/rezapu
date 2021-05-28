254a06722138f65015868ad608b4e12f6e086ce6
obediqbal <obed.ubaidillah@gmail.com>
2021-05-29 01:57:11 +0700

    calculator 0.1

--- calculator/assets/kalkulator.js
+++ calculator/assets/kalkulator.js
@@ -1,1 +1,32 @@
-console.log("Selamat Anda berhasil menggunakan JavaScript pada webstie")
+const calculator = {
+    displayNumber: '0',
+    operator: null,
+    firstNumber: null,
+    waitingForSecondNumber: false
+};
+
+function updateDisplay(){
+    document.querySelector("#displayNumber").innerText = calculator.displayNumber;
+}
+
+function clearCalculator(){
+    calculator.displayNumber = '0';
+    calculator.operator = null;
+    calculator.firstNumber = null;
+    calculator.waitingForSecondNumber = false;
+}
+
+function inputDigit(digit){
+    calculator.displayNumber += digit;
+}
+
+const buttons = document.querySelectorAll(".button");
+
+for(let button of buttons){
+    button.addEventListener('click',function(event){
+        const target = event.target;
+
+        inputDigit(target.innerText);
+        updateDisplay();
+    });
+}
