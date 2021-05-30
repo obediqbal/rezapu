70ee7024ddfc45e94f909009fd87421047ea66be
obediqbal <obed.ubaidillah@gmail.com>
2021-05-30 13:12:36 +0700

    working color palette

--- assets/styles/style.js
+++ assets/styles/style.js
@@ -1,2 +1,10 @@
-let palette = document.querySelector(".palette");
-let colors = palette.querySelectorAll(".color");
+let colors = document.querySelectorAll(".color");
+
+for(color of colors){
+    color.addEventListener('click',function(event){
+        let listBase = document.querySelectorAll(".base-color")
+        for(usesBase of listBase){
+            usesBase.style.setProperty('--base-color',getComputedStyle(event.target).getPropertyValue('background-color'));
+        }
+    });
+}
