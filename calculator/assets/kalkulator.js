733959ac0da23357c9e0a6b457ccd7f7a88d1f19
obediqbal <obed.ubaidillah@gmail.com>
2021-05-30 18:54:14 +0700

    calculator v0,3

--- calculator/assets/kalkulator.js
+++ calculator/assets/kalkulator.js
@@ -56,7 +56,16 @@ function performCalculation(){
         result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
     }
 
+    const history={
+        firstNumber: calculator.firstNumber,
+        secondNumber: calculator.displayNumber,
+        operator: calculator.operator,
+        result: result
+    }
+
+    putHistory(history);
     calculator.displayNumber = result;
+    renderHistory();
 }
 
 const buttons = document.querySelectorAll(".button");
