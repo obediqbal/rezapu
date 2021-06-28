06a006b08413e9863b3d02c446eee639de56d579
obediqbal <obed.ubaidillah@gmail.com>
2021-06-28 12:39:32 +0700

    "add function addBook to List"

--- submissionApp/assets/addForm.js
+++ submissionApp/assets/addForm.js
@@ -22,40 +22,60 @@ function showSuccessMessage() {
 }
 
 function statusToInt() {
-    if(sedangStatus.checked) return 0;
-    else if(selesaiStatus.checked) return 1;
+    if (sedangStatus.checked) return 0;
+    else if (selesaiStatus.checked) return 1;
     else return 2;
+}
+
+function statusToString() {
+    const sint = statusToInt();
+    if(sint==0) return "Sedang Dibaca";
+    else if(sint==1) return "Selesai Dibaca";
+    else return "Akan Dibaca";
 }
 
 submitForm.addEventListener("submit", function (event) {
     event.preventDefault();
+    const judul = judulBuku.value;
+    const penulis = penulisBuku.value;
+    const tahun = tahunBuku.value;
+    const statusString = statusToString();
+    const status = statusToInt();
+    const rate = ratingBuku.value;
 
-    const buku = composeBukuObject(judulBuku.value, penulisBuku.value, tahunBuku.value, statusToInt(), ratingBuku.value)
-    bukubuku.push(buku);
+    const buku = makeBuku(judul,penulis,tahun,statusString,rate);
+    const bukuObject = composeBukuObject(judul, penulis, tahun, statusString, rate);
+
+    bukubuku.push(bukuObject);
+    buku[BOOK_ITEM_ID] = bukuObject.id;
     console.log(bukubuku);
+
+    if(status==0) uncompletedBookList.append(buku);
+    else if(status==1) completedBookList.append(buku);
+    else plannedBookList.append(buku);
 
     showSuccessMessage();
     submitForm.reset();
 });
 
-for(let i=0;i<3;i++){
+for (let i = 0; i < 3; i++) {
     let target = submitForm.children[i].children[2];
     let toChange = preview.children[i].children[0];
-    target.addEventListener("input",function(){
+    target.addEventListener("input", function () {
         toChange.innerText = target.value;
 
-        if(target.value=="" || target==tahunBuku && (parseInt(target.value)>target.max || parseInt(target.value)<target.min)) {
+        if (target.value == "" || target == tahunBuku && (parseInt(target.value) > target.max || parseInt(target.value) < target.min)) {
             toChange.innerText = "-";
         }
     });
 
     let target2 = submitForm.children[3];
-    target2.children[i*2].addEventListener("input",function(){
-        preview.children[3].children[0].innerText = target2.children[(i*2)+1].innerText;
+    target2.children[i * 2].addEventListener("input", function () {
+        preview.children[3].children[0].innerText = statusToString(i);
     });
 }
 
-ratingBuku.addEventListener("input",function(){
+ratingBuku.addEventListener("input", function () {
     let target = document.getElementById("ratePrev").children[0];
     target.innerText = ratingBuku.value;
 });
---
+++ submissionApp/assets/dom.js
@@ -0,0 +1,47 @@
+const uncompletedBookList = document.getElementById("sedangPanel");
+const completedBookList = document.getElementById("selesaiPanel");
+const plannedBookList = document.getElementById("akanPanel");
+const BOOK_ITEM_ID = "bookId"
+
+function createButton(){
+
+}
+
+function createEditButton(){
+
+}
+
+function createRemoveButton(){
+
+}
+
+function makeBuku(judul,penulis,tahun,status,rate){
+    const titleText = document.createElement("span");
+    titleText.innerText = judul
+
+    const rating = document.createElement("span");
+    rating.innerText = "("+rate+") ";
+
+    const title = document.createElement("h4");
+    title.append(rating);
+    title.append(titleText);
+
+    const author = document.createElement("p");
+    author.innerText = penulis;
+
+    const year = document.createElement("p");
+    year.innerText = tahun;
+
+    const statusBuku = document.createElement("p");
+    statusBuku.innerText = status;
+    
+    const textContainer = document.createElement("div");
+    textContainer.append(title, author, year, statusBuku);
+
+    const container = document.createElement("div");
+    container.append(textContainer);
+    container.append(createEditButton());
+    container.append(createRemoveButton());
+
+    return container;
+}
