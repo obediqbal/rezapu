3244a854127707bc808df510f7564615f99f7a5a
obediqbal <obed.ubaidillah@gmail.com>
2021-07-03 21:58:09 +0700

    add WebStorage to Nav

--- submissionApp/assets/nav.js
+++ submissionApp/assets/nav.js
@@ -6,25 +6,35 @@ const addPanel = document.getElementById("addPanel");
 const rakPanel = document.getElementById("rakPanel");
 const rakList = document.getElementById("rakListNav")
 const rakNav = document.getElementById("rakNav");
+
+const sedangCheck = rakList.children[0].children[0];
+const selesaiCheck = rakList.children[1].children[0];
+const akanCheck = rakList.children[2].children[0];
+
 const sedangPanel = document.getElementById("sedangPanel");
 const selesaiPanel = document.getElementById("selesaiPanel");
 const akanPanel = document.getElementById("akanPanel");
 
 let navObject = {
-    currentDisplay,
-    isUncompletedChecked,
-    isCompletedChecked,
-    isPlannedChecked
+    currentDisplay : "rak",
+    isUncompletedChecked : false,
+    isCompletedChecked : false,
+    isPlannedChecked : false
 }
 
 addNavButton.addEventListener("click", () => {
-    changeFocusTo(addNavButton);
-    addPanel.removeAttribute("hidden");
+    navObject.currentDisplay = "tambah";
+
+    updateDisplayToStorage();
+    refreshDisplay();
 });
 
 rakNavButton.addEventListener("click", () => {
-    changeFocusTo(rakNav);
-    rakPanel.removeAttribute("hidden");
+    navObject.currentDisplay = "rak";
+    
+    updateDisplayToStorage();
+    refreshDisplay();
+
     rakList.classList.toggle("show");
 });
 
@@ -32,10 +42,10 @@ function panelReset() {
     addPanel.setAttribute("hidden", true);
     rakPanel.setAttribute("hidden", true);
     resetAddMessage();
-    if(heading.innerText == EDIT_BOOK_HEADING) modeToAdd();
+    if (heading.innerText == EDIT_BOOK_HEADING) modeToAdd();
 }
 
-function onFocusReset(){
+function onFocusReset() {
     rakNav.classList.remove("onFocusBg");
     addNavButton.classList.remove("onFocusBg");
 }
@@ -47,39 +57,81 @@ function changeFocusTo(element) {
 
     element.classList.add("onFocusBg");
 
-    if(element!=rakNav){
+    if (element != rakNav) {
         rakList.classList.remove("show");
     }
 }
 
-rakList.addEventListener("input", ()=>{
-    let i=0
-    while(i<3){
-        let target = rakList.children[i].children[0];
-        if(target.checked){
-            rakPanel.children[1].children[i].removeAttribute("hidden");
-            if(i==0) navObject.isUncompletedChecked=true;
-            else if(i==1) navObject.isCompletedChecked=true;
-            else navObject.isPlannedChecked=true;
-        }
-        else{
-            rakPanel.children[1].children[i].setAttribute("hidden",true);
-            if(i==0) navObject.isUncompletedChecked=false;
-            else if(i==1) navObject.isCompletedChecked=false;
-            else navObject.isPlannedChecked=false;
-        }
-        i++;
-    }
+sedangCheck.addEventListener("input", function(){
+    if(sedangCheck.checked) navObject.isUncompletedChecked=true;
+    else navObject.isUncompletedChecked=false;
+
+    updateDisplayToStorage();
+    refreshDisplay();
 });
 
-function loadDisplay(){
+selesaiCheck.addEventListener("input", function(){
+    if(selesaiCheck.checked) navObject.isCompletedChecked=true;
+    else navObject.isCompletedChecked=false;
 
+    updateDisplayToStorage();
+    refreshDisplay();
+});
+
+akanCheck.addEventListener("input", function(){
+    if(akanCheck.checked) navObject.isPlannedChecked=true;
+    else navObject.isPlannedChecked=false;
+
+    updateDisplayToStorage();
+    refreshDisplay();
+});
+
+function refreshDisplay() {
+    if(navObject.currentDisplay=="rak") {
+        changeFocusTo(rakNav);
+        rakPanel.removeAttribute("hidden");
+    }
+    else if(navObject.currentDisplay=="tambah") {
+        changeFocusTo(addNavButton);
+        addPanel.removeAttribute("hidden");
+    }
+    if(navObject.isUncompletedChecked) {
+        sedangCheck.checked=true;
+        sedangPanel.removeAttribute("hidden");
+    }
+    else {
+        sedangCheck.checked=false;
+        sedangPanel.setAttribute("hidden", true);
+    }
+    if(navObject.isCompletedChecked) {
+        selesaiCheck.checked=true;
+        selesaiPanel.removeAttribute("hidden");
+    }
+    else {
+        selesaiCheck.checked=false;
+        selesaiPanel.setAttribute("hidden", true);
+    }
+    if(navObject.isPlannedChecked) {
+        akanCheck.checked=true;
+        akanPanel.removeAttribute("hidden");
+    }
+    else {
+        akanCheck.checked=false;
+        akanPanel.setAttribute("hidden", true);
+    }
 }
 
-function saveDisplay(){
-    
+function loadDisplay() {
+    let item = JSON.parse(localStorage.getItem(NAV_KEY));
+    if (item != null) navObject = item;
+    refreshDisplay();
 }
 
-function updateDisplayToStorage(){
-    if(isStorageExist()) saveDisplay();
+function saveDisplay() {
+    let item = JSON.stringify(navObject)
+    localStorage.setItem(NAV_KEY, item);
+}
+
+function updateDisplayToStorage() {
+    if (isStorageExist()) saveDisplay();
 }
